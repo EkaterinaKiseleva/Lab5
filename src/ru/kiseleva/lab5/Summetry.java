2c04ba2b190f6d64a8c6cf3ffa16c6665c6ae7da
EkaterinaKiseleva <katebaranova@bk.ru>
2013-02-05 23:09:13 +0700

    first commit

---
+++ src/ru/kiseleva/lab5/Summetry.java
@@ -0,0 +1,6 @@
+package ru.kiseleva.lab5;
+
+public interface Summetry {
+
+    void summet();
+}
---
+++ src/ru/kiseleva/lab5/TranslationByVektor.java
@@ -0,0 +1,6 @@
+package ru.kiseleva.lab5;
+
+public interface TranslationByVektor {
+
+    void transfer(Vektor p);
+}
